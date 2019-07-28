a977049e742747143423adecc3790c8746b4e3df
Alex Shepherd <kiwi64ajs@gmail.com>
2019-07-29 02:48:00 +1200

    changed HIGH fuse to disable JTAG added "bootloader.tool" entry so the bootloader can be programmed added Arduino Pin Number to pins_arduino.h file to make it easier to figure out which port/pin the mappings are to

--- variants/Io/pins_arduino.h
+++ variants/Io/pins_arduino.h
@@ -107,115 +107,115 @@ const uint16_t PROGMEM port_to_input_PGM[] = {
 };
 
 const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
-	PA, /* 0 OUTPUTS */
-	PA, /* 1 */
-	PA, /* 2 */
-	PA, /* 3 */
-	PA, /* 4 */
-	PA, /* 5 */
-	PA, /* 6 */
-	PA, /* 7 */
-	PC, /* 8 INPUTS */
-	PC, /* 9 */
-	PC, /* 10 */
-	PC, /* 11 */
-	PC, /* 12 */
-	PC, /* 13 */
-	PC, /* 14 */
-	PC, /* 15 */
-	PB, /* 16 PORT B*/
-	PB, /* 17 */
-	PB, /* 18 */
-	PB, /* 19 */
-	PB, /* 20 */
-	PB, /* 21 */
-	PB, /* 22 */
-	PB, /* 23 */
-	PD, /* 24 PORT D/G */
-	PD, /* 25 */
-	PD, /* 26 */
-	PD, /* 27 */
-	PD, /* 28 */
-	PG, /* 29 PD5 is CAN: Sub in PG3 here */
-	PG, /* 30 PD6 is CAN: Sub in PG4 here */
-	PD, /* 31 */
-	PE, /* 32 PORT E */
-	PE, /* 33 */
-	PE, /* 34 */
-	PE, /* 35 */
-	PE, /* 36 */
-	PE, /* 37 */
-	PE, /* 38 */
-	PE, /* 39 */
-	PF, /* 40 PORT F */
-	PF, /* 41 */
-	PF, /* 42 */
-	PF, /* 43 */
-	PF, /* 44 */
-	PF, /* 45 */
-	PF, /* 46 */
-	PF, /* 47 */
-	PG, /* 48 BLUE */
-	PG, /* 49 GOLD */
+	PA, /* Arduino Pin  0 OUTPUTS */
+	PA, /* Arduino Pin  1 */
+	PA, /* Arduino Pin  2 */
+	PA, /* Arduino Pin  3 */
+	PA, /* Arduino Pin  4 */
+	PA, /* Arduino Pin  5 */
+	PA, /* Arduino Pin  6 */
+	PA, /* Arduino Pin  7 */
+	PC, /* Arduino Pin  8 INPUTS */
+	PC, /* Arduino Pin  9 */
+	PC, /* Arduino Pin 10 */
+	PC, /* Arduino Pin 11 */
+	PC, /* Arduino Pin 12 */
+	PC, /* Arduino Pin 13 */
+	PC, /* Arduino Pin 14 */
+	PC, /* Arduino Pin 15 */
+	PB, /* Arduino Pin 16 PORT B*/
+	PB, /* Arduino Pin 17 */
+	PB, /* Arduino Pin 18 */
+	PB, /* Arduino Pin 19 */
+	PB, /* Arduino Pin 20 */
+	PB, /* Arduino Pin 21 */
+	PB, /* Arduino Pin 22 */
+	PB, /* Arduino Pin 23 */
+	PD, /* Arduino Pin 24 PORT D/G */
+	PD, /* Arduino Pin 25 */
+	PD, /* Arduino Pin 26 */
+	PD, /* Arduino Pin 27 */
+	PD, /* Arduino Pin 28 */
+	PG, /* Arduino Pin 29 PD5 is CAN: Sub in PG3 here */
+	PG, /* Arduino Pin 30 PD6 is CAN: Sub in PG4 here */
+	PD, /* Arduino Pin 31 */
+	PE, /* Arduino Pin 32 PORT E */
+	PE, /* Arduino Pin 33 */
+	PE, /* Arduino Pin 34 */
+	PE, /* Arduino Pin 35 */
+	PE, /* Arduino Pin 36 */
+	PE, /* Arduino Pin 37 */
+	PE, /* Arduino Pin 38 */
+	PE, /* Arduino Pin 39 */
+	PF, /* Arduino Pin 40 PORT F */
+	PF, /* Arduino Pin 41 */
+	PF, /* Arduino Pin 42 */
+	PF, /* Arduino Pin 43 */
+	PF, /* Arduino Pin 44 */
+	PF, /* Arduino Pin 45 */
+	PF, /* Arduino Pin 46 */
+	PF, /* Arduino Pin 47 */
+	PG, /* Arduino Pin 48 BLUE */
+	PG, /* Arduino Pin 49 GOLD */
 };
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
-	_BV(0), /* 0, port A */
-	_BV(1), /* 1, port A */
-	_BV(2), /* 2, port A */
-	_BV(3), /* 3, port A */
-	_BV(4), /* 4, port A */
-	_BV(5), /* 5, port A */
-	_BV(6), /* 6, port A */
-	_BV(7), /* 7, port A */
+	_BV(0), /* 0, port A Arduino Pin  0 */
+	_BV(1), /* 1, port A Arduino Pin  1 */
+	_BV(2), /* 2, port A Arduino Pin  2 */
+	_BV(3), /* 3, port A Arduino Pin  3 */
+	_BV(4), /* 4, port A Arduino Pin  4 */
+	_BV(5), /* 5, port A Arduino Pin  5 */
+	_BV(6), /* 6, port A Arduino Pin  6 */
+	_BV(7), /* 7, port A Arduino Pin  7 */
 	
-	_BV(0), /* 0, port C */
-	_BV(1), /* 1, port C */
-	_BV(2), /* 2, port C */
-	_BV(3), /* 3, port C */
-	_BV(4), /* 4, port C */
-	_BV(5), /* 5, port C */
-	_BV(6), /* 6, port C */
-	_BV(7), /* 7, port C */
+	_BV(0), /* 0, port C Arduino Pin  8 */
+	_BV(1), /* 1, port C Arduino Pin  9 */
+	_BV(2), /* 2, port C Arduino Pin 10 */
+	_BV(3), /* 3, port C Arduino Pin 11 */
+	_BV(4), /* 4, port C Arduino Pin 12 */
+	_BV(5), /* 5, port C Arduino Pin 13 */
+	_BV(6), /* 6, port C Arduino Pin 14 */
+	_BV(7), /* 7, port C Arduino Pin 15 */
 	
-	_BV(0), /* 0, port B */
-	_BV(1), /* 1, port B */
-	_BV(2), /* 2, port B */
-	_BV(3), /* 3, port B */
-	_BV(4), /* 4, port B */
-	_BV(5), /* 5, port B */
-	_BV(6), /* 6, port B */
-	_BV(7), /* 7, port B */
+	_BV(0), /* 0, port B Arduino Pin 16 */
+	_BV(1), /* 1, port B Arduino Pin 17 */
+	_BV(2), /* 2, port B Arduino Pin 18 */
+	_BV(3), /* 3, port B Arduino Pin 19 */
+	_BV(4), /* 4, port B Arduino Pin 20 */
+	_BV(5), /* 5, port B Arduino Pin 21 */
+	_BV(6), /* 6, port B Arduino Pin 22 */
+	_BV(7), /* 7, port B Arduino Pin 23 */
 	
-	_BV(0), /* 0, port D */
-	_BV(1), /* 1, port D */
-	_BV(2), /* 2, port D */
-	_BV(3), /* 3, port D */
-	_BV(4), /* 4, port D */
-	_BV(3), /* 3, port G */
-	_BV(4), /* 4, port G */
-	_BV(7), /* 7, port D */
+	_BV(0), /* 0, port D Arduino Pin 24 */
+	_BV(1), /* 1, port D Arduino Pin 25 */
+	_BV(2), /* 2, port D Arduino Pin 26 */
+	_BV(3), /* 3, port D Arduino Pin 27 */
+	_BV(4), /* 4, port D Arduino Pin 28 */
+	_BV(3), /* 3, port G Arduino Pin 29 */
+	_BV(4), /* 4, port G Arduino Pin 30 */
+	_BV(7), /* 7, port D Arduino Pin 31 */
 	
-	_BV(0), /* 0, port E */
-	_BV(1), /* 1, port E */
-	_BV(2), /* 2, port E */
-	_BV(3), /* 3, port E */
-	_BV(4), /* 4, port E */
-	_BV(5), /* 5, port E */
-	_BV(6), /* 6, port E */
-	_BV(7), /* 7, port E */
+	_BV(0), /* 0, port E Arduino Pin 32 */
+	_BV(1), /* 1, port E Arduino Pin 33 */
+	_BV(2), /* 2, port E Arduino Pin 34 */
+	_BV(3), /* 3, port E Arduino Pin 35 */
+	_BV(4), /* 4, port E Arduino Pin 36 */
+	_BV(5), /* 5, port E Arduino Pin 37 */
+	_BV(6), /* 6, port E Arduino Pin 38 */
+	_BV(7), /* 7, port E Arduino Pin 39 */
 
-	_BV(0), /* 0, port F */
-	_BV(1), /* 1, port F */
-	_BV(2), /* 2, port F */
-	_BV(3), /* 3, port F */
-	_BV(4), /* 4, port F */
-	_BV(5), /* 5, port F */
-	_BV(6), /* 6, port F */
-	_BV(7), /* 7, port F */
+	_BV(0), /* 0, port F Arduino Pin 40 */
+	_BV(1), /* 1, port F Arduino Pin 41 */
+	_BV(2), /* 2, port F Arduino Pin 42 */
+	_BV(3), /* 3, port F Arduino Pin 43 */
+	_BV(4), /* 4, port F Arduino Pin 44 */
+	_BV(5), /* 5, port F Arduino Pin 45 */
+	_BV(6), /* 6, port F Arduino Pin 46 */
+	_BV(7), /* 7, port F Arduino Pin 47 */
 	
-	_BV(0), /* 0, port G BLUE */
-	_BV(1), /* 1, port G GOLD */
+	_BV(0), /* 0, port G BLUE Arduino Pin 48 */
+	_BV(1), /* 1, port G GOLD Arduino Pin 49 */
 };
 
 
